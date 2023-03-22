e779e60a3a1740a3f195b411999f659ae22ec2e1
zobakellygilber <kellyncodes@gmail.com>
2023-03-22 11:34:09 +0100

    feat: Database setup with efcore

---
+++ SimpleCollab.Data/Context/ApiDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCollab.Models.Entities;
+
+namespace SimpleCollab.Data.Context
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(prop => prop.Address).IsRequired(false);
+                entity.HasIndex(prop => prop.Email, $"IX_{nameof(User.Email)}").IsUnique(true);
+                entity.HasIndex(prop => prop.Id, $"IX_{nameof(User.Id)}").IsUnique(true);
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SimpleCollab.Data/Migrations/20230322102704_Intial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleCollab.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Id",
+                table: "Users",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
